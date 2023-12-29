df2d450ce05abfe932024856173dbc040e903a4a
Shreyakulkarni2022 <155136839+Shreyakulkarni2022@users.noreply.github.com>
2023-12-29 12:05:16 +0530

    Add files via upload

---
+++ module3.py.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+import from module1
+print(module1.greet("hello"))
+print(module1.add(3,5))
+print(module.minus(8,5))
+print(module.pi)
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+
+
+
+# In[12]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
